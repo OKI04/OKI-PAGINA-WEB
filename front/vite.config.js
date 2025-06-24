cf465b7028a398f7230f5babce5048d420750f42
OKI04 <okibasedata@gmail.com>
2025-06-23 20:18:28 -0500

    Front completo

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,21 +1,24 @@
 import { defineConfig } from 'vite';
-import { VitePluginStaticCopy } from 'vite-plugin-static-copy';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { resolve } from 'path';
-import VitePluginMultiPage from 'vite-plugin-multi-page';
 
 export default defineConfig({
   base: './',
-  plugins: [
-    VitePluginMultiPage()
-  ],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        dashboard: resolve(__dirname, 'views/dashboardAdmin.html')
+        admin: resolve(__dirname, 'src/views/dashboardAdmin.html')
       }
     },
-    outDir: 'dist',
+    outDir: 'dist'
   },
+  server: {
+    proxy: {
+      '/admin': {
+        target: 'https://backend-oki-web.onrender.com',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 });
