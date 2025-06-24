fa4a631abe53ad947869ebfd651bbb56126d8a5f
OKI04 <okibasedata@gmail.com>
2025-06-23 20:00:49 -0500

    Front casi listo

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite';
+import { VitePluginStaticCopy } from 'vite-plugin-static-copy';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { resolve } from 'path';
+import VitePluginMultiPage from 'vite-plugin-multi-page';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/admin': {
-        target: 'https://backend-oki-web.onrender.com',  // tu backend Node
-        changeOrigin: true,
-        secure: false
+  base: './',
+  plugins: [
+    VitePluginMultiPage()
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dashboard: resolve(__dirname, 'views/dashboardAdmin.html')
       }
-    }
-  }
+    },
+    outDir: 'dist',
+  },
 });
